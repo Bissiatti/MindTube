190d0765930220416d588bdb36dd098de1a8054f
Bissiatti <emanuelbissiatti@outlook.com.br>
2022-11-04 15:21:30 -0300

    add search get

--- public/js/search.js
+++ public/js/search.js
@@ -5,9 +5,25 @@ class Search{
 		this.search.setAttribute('placeholder', 'Cole aqui o link do vídeo');	
 		this.search.setAttribute('id', 'search');
 
+		this.sendSearch();
+
 	}
 
 	appendSearch(app){
 		app.appendChild(this.search);
+	}
+	
+	sendSearch(){
+		this.search.addEventListener('keypress', (e) => {
+			if(e.key == 'Enter'){
+				console.log(this.search.value);
+				fetch('http://localhost:3000/api/search/' + this.search.value, {
+					method: 'GET',
+				}).then
+				(response => response.json()).then(data => {
+					console.log(data);
+				});
+			}
+		});
 	}
 }
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -5,3 +5,8 @@ export const router = Router()
 router.get('/',(req,res)=>{
 	console.log("yes")
 })
+
+router.get('/api/search/:data',(req,res)=>{
+	console.log(req)
+	res.send({"test":"test"})
+})
