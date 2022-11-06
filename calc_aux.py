af807b9f69059aed0c47917431b89ae6d538e567
OsmarCLFilho <osmarclopesfilho@gmail.com>
2022-11-06 18:20:59 -0300

    calculate and calc_aux scripts

---
+++ calc_aux.py
@@ -0,0 +1,134 @@
+import sys
+import numpy as np
+import pandas as pd
+from youtube_transcript_api import YouTubeTranscriptApi
+import re
+import itertools
+from nltk import FreqDist, bigrams, pos_tag, word_tokenize
+import networkx as nx
+from pyvis.network import Network
+from collections import Counter
+from nltk.tokenize.toktok import ToktokTokenizer
+from nltk.corpus import stopwords
+from youtube_transcript_api import YouTubeTranscriptApi
+from gensim.models import KeyedVectors
+from nltk import download as nltkdownload
+
+toktok = ToktokTokenizer()
+stpwrd = stopwords.words('english')
+stop_list = (['uh', 'also', 'oh', 'um', 'yeah', 'use', 'lot', 'put', 'get', 'would', 'gonna', 'really', 'much', 'actually', 'another'])
+stpwrd.extend(stop_list)
+
+
+modelo = KeyedVectors.load("./data/modelo_word_vec.kv")
+
+def getSimilar(string, N):
+    similarList = modelo.most_similar(positive=[string])[0:N]
+    wordList = []
+    for word in similarList:
+        wordList.append(word)
+    return wordList
+
+def sort_list(list1, list2):
+     
+    list3 = []
+    for entry in list2:
+        list3.append(entry[1])
+    zipped_pairs = zip(list3, list1)
+ 
+    z = [x for _, x in sorted(zipped_pairs, reverse = True)]
+ 
+    return z
+
+def banStopword(lista, n):
+    SUSwords = []
+    similarWords = []
+    for word in lista:
+        similares = getSimilar(word,n)
+        mostSimilar = 0
+        similarWord = ''
+        SUSWord = ''
+        for palavra in similares:
+            sentinel = 0
+            if palavra[0] in stpwrd and palavra[1] > mostSimilar:
+                mostSimilar = palavra[1]
+                similarWord = palavra
+                SUSWord = word
+                sentinel = 1
+            if sentinel == 1:  
+                SUSwords.append(word)
+                similarWords.append(palavra)
+    
+    return SUSwords, similarWords
+
+def ejectStopword(string, n):
+    State = False
+    try:
+        similares = getSimilar(string.lower(),n)
+        for palavra in similares:
+            if palavra[0] in stpwrd:
+                    State = True
+    except KeyError:
+        pass
+    
+    return State
+
+def filtered_sentence(example_sent):
+    word_tokens = toktok.tokenize(example_sent)
+    filtered_sentence = [w for w in word_tokens if not w.lower() in stpwrd]
+    return " ".join(filtered_sentence)
+
+def remove_stopwords(text):
+    doc = filtered_sentence(text)
+    return " ".join([token.text for token in doc if token.is_stop == False])
+
+def pre_processing(text):
+    text = filtered_sentence(text)
+    text = re.sub(r'[^\w\s]', '', text) #remove punctuation
+    text = re.sub(r"\s{2,}", " ", text)
+    return text
+
+def co_occurrence(text):
+    corpus = list(itertools.chain.from_iterable([text.split(' ')]))
+    vocab = list(set(corpus))
+    vocab_index = {word: i for i, word in enumerate(vocab)}
+    bi_grams = list(bigrams(corpus))
+    bigram_freq = FreqDist(bi_grams).most_common(len(bi_grams)) 
+    co_occurrence_matrix = np.zeros((len(vocab), len(vocab)))
+
+    #bigram = ((word n, word n+1), num_occurrences)
+    for bigram in bigram_freq:
+        current = bigram[0][1] #word n+1
+        previous = bigram[0][0] #word n
+        count = bigram[1] #num_occurrences
+        pos_current = vocab_index[current] #obtain id for word n+1 
+        pos_previous = vocab_index[previous] #obtain id for word n 
+        co_occurrence_matrix[pos_current][pos_previous] = count
+        
+    return co_occurrence_matrix, vocab_index
+
+def structure_text_network(matrix, vocab_index):
+    data_matrix = pd.DataFrame(matrix, index=vocab_index, columns=vocab_index)
+    data_stack = data_matrix.stack()
+    structure = data_stack[data_stack >= 1].rename_axis(('source', 'target')).reset_index(name='weight')
+    return structure[(structure.source != " ") & (structure.target != " ")]
+
+def calc_centrality(G, title):
+    title_words = pre_processing(title).split()
+    centrality =  nx.pagerank(G)
+    
+    def mean_simi(word,title):
+        m = 0
+        for i in title:
+            try:
+                m += modelo.similarity(word, i)
+            except KeyError:
+                m += 0
+
+        return m/len(title)
+
+    centrality = {x[0]:(x[1]**0.5)*
+                  mean_simi(x[0],title_words)/(mean_simi(x[0],stpwrd)+0.000001)
+                  for x in centrality.items()}
+
+    return sorted(centrality.items(), key=lambda x: x[1], reverse=False)
--- calculate.py
+++ calculate.py
@@ -1,11 +1,27 @@
 import sys
 from youtube_transcript_api import YouTubeTranscriptApi
+import re
+import itertools
+from nltk import FreqDist, bigrams, pos_tag, word_tokenize
+import networkx as nx
+from pyvis.network import Network
+from collections import Counter
+from nltk.tokenize.toktok import ToktokTokenizer
+from nltk.corpus import stopwords
+from youtube_transcript_api import YouTubeTranscriptApi
+from gensim.models import KeyedVectors
+from nltk import download as nltkdownload
+from calc_aux import *
 
-id = sys.argv[1]
+is_noun = lambda word: pos_tag(word_tokenize(word))[0][1] == 'NN'
+
+#sys args
+iden = sys.argv[1]
 title = sys.argv[2]
 
-def generate_transcript(id):
-	transcript = YouTubeTranscriptApi.get_transcript(id,languages=['en'])
+#get transcript
+def generate_transcript(iden):
+	transcript = YouTubeTranscriptApi.get_transcript(iden,languages=['en'])
 	script = ""
 
 	for text in transcript:
@@ -13,8 +29,122 @@ def generate_transcript(id):
 		if t != '[Music]':
 			script += t + " "
 		
-	return script, len(script.split())
+	return script
 
+transcript = generate_transcript(iden)
 
-transcript, no_of_words = generate_transcript(id)
-print(title)
+transcript2 = YouTubeTranscriptApi.get_transcript(iden,languages=['en'])
+
+# dicionário do momento de cada fala
+legend_dict = {}
+for text in transcript2:
+    legend_dict[text['start']] = text['text']
+
+text = transcript
+
+text_cleaned = pre_processing(text)
+matrix, vocab = co_occurrence(text_cleaned)
+structure_network = structure_text_network(matrix, vocab)
+
+#GambiaRank and stuff
+text_network = nx.DiGraph()
+text_network = nx.from_pandas_edgelist(structure_network, 'target', 'source', ['weight'])
+for i in text_network.nodes:
+    for j in text_network.nodes:
+        if i != j and i.lower() == j.lower():
+            text_network = nx.contracted_nodes(text_network,i, j)
+
+centrality = calc_centrality(text_network, title)
+
+cen_dict = {}
+max_cen = 0
+for x in centrality:
+    cen_dict[x[0]] = x[1]
+    if max_cen < x[1]:
+        max_cen = x[1]
+
+# Visualização do Grafo
+# lista dos nós
+name_lst = [x[0] for x in centrality]
+
+# lista de nós a serem removidos, estamos mantendo os 25 maiores
+rmv_lst = name_lst[:-40]
+
+# exibição do nome dos nós para a visualização
+for i in text_network.nodes:
+    text_network.nodes[i]['label'] = i
+
+# redução do grafo
+for i in rmv_lst:
+    text_network.remove_node(i)
+    
+name_lst = list(text_network.nodes)
+
+# remoção de stopwords
+for i in name_lst:
+    if ejectStopword(i,5) or not is_noun(i):
+        merge_node = False
+        for j in text_network.neighbors(i):
+            maxi = 0
+            w = name_lst.index(j)
+            if maxi < w and (not (ejectStopword(j,5) or not is_noun(j))):
+                maxi = w
+                merge_node = j
+            for j in text_network.neighbors(i):
+                if merge_node:
+                    if not (j == merge_node or (j in text_network.neighbors(merge_node))):
+                        text_network.add_edge(merge_node, j, weight=text_network[i][j]['weight']/2)
+        text_network.remove_node(i)
+
+dict_moments = {}
+episilon = 45
+
+for i in text_network.nodes:
+    dict_moments[i] = []
+    
+for i in list(legend_dict.keys()):
+    phrase = pre_processing(legend_dict[i])
+    
+    for j in text_network.nodes:
+        if j in phrase.split() and (len(dict_moments[j]) == 0 or  dict_moments[j][-1]+episilon<i):
+            dict_moments[j].append((i>1)*(int(i)-1))
+
+nt = Network('800px', '1000px', bgcolor='#222222', notebook=True)
+nt.from_nx(text_network)
+nt.force_atlas_2based(gravity = -200)
+for edg in nt.edges:
+    # espessura e física das arestas
+    #edg['width'] = text_network[i][j]['weight']**4
+    #if edg['weight'] < 1:
+    
+    if edg['from'] == edg['to']:
+        edg['hidden'] = True
+    edg['physics'] = True
+    edg['color'] = '#cf888f'
+
+for n in nt.nodes:
+    # tamanho dos nós
+    n['shape'] = 'ellipse'
+    size = 40*(cen_dict[n['label']]/max_cen)**0.4
+    n['font'] = str(int(size))+'px arial white'
+    n['color'] = '#7f333f'
+    n['labelHighlightBold'] = True
+
+def time_to_min(time):
+    return str(time//60)+":"+"0"*(time%60<10)+str(time%60)
+
+for n in nt.nodes:
+    # links nos nós
+    '''
+    link_list = ["<a href='https://www.youtube.com/watch?v="+str(iden)+
+                 "&t="+str(time)+"s' target='_blank' rel='noopener noreferrer'>"+time_to_min(time)+"<br>" for time in dict_moments[n['label']]]
+    '''
+
+    link_list = ['"<button type="button">'+str(time) +'</button>"' for time in dict_moments[n["label"]]]
+
+    soma = ''
+    for k in link_list:
+        soma += k
+    n['title'] = soma
+    
+nt.show("text_network.html")
